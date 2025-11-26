f186ae275af8933ec78b7de21468d5802e52ba2f
lytharius <lytharius.4@gmail.com>
2025-11-26 21:31:02 +0100

    integrated groups in main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 from player import Player # type: ignore
 
 
+
+
 def main():
 
     print("Starting Asteroids with pygame version:", pygame.version.ver)
@@ -13,10 +15,15 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+   
+    updatable: pygame.sprite.Group = pygame.sprite.Group() 
+    drawable: pygame.sprite.Group = pygame.sprite.Group() 
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    dt = 0
+    
     while True:
         log_state()
 
@@ -24,10 +31,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
+
 
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+     
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
